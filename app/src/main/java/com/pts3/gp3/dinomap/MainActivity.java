be630865897a7507a9ff5ffb1abf27a01775c1a4
Rémi Audrézet <AR2000AR@users.noreply.github.com>
2019-09-16 14:13:23 +0200

    Initial commit

---
+++ app/src/main/java/com/pts3/gp3/dinomap/MainActivity.java
@@ -0,0 +1,14 @@
+package com.pts3.gp3.dinomap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
