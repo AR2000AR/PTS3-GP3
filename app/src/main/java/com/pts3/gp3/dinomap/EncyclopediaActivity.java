8f4cbc35f23e911c6db3c81ccb7eb9e1ca6ab6cd
Rémi Audrézet <AR2000AR@users.noreply.github.com>
2019-09-27 11:57:34 +0200

    Link between (world) activty

---
+++ app/src/main/java/com/pts3/gp3/dinomap/EncyclopediaActivity.java
@@ -0,0 +1,14 @@
+package com.pts3.gp3.dinomap;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EncyclopediaActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_encyclopedia);
+    }
+}
--- app/src/main/java/com/pts3/gp3/dinomap/MainActivity.java
+++ app/src/main/java/com/pts3/gp3/dinomap/MainActivity.java
@@ -2,9 +2,13 @@ package com.pts3.gp3.dinomap;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,7 +17,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
        // setContentView(R.layout.activity_main);
-        setContentView(R.layout.activity_encyclopedia);
+        setContentView(R.layout.activity_main);
+
+        LinearLayout mapLayout = findViewById(R.id.mapLayout);
+        LinearLayout encycloLayout = findViewById(R.id.encycloLayout);
+        LinearLayout quizLayout = findViewById(R.id.quizLayout);
+        ImageButton mapIcone = findViewById(R.id.mapIcone);
+        ImageButton encycloIcone = findViewById(R.id.encycloIcone);
+        ImageButton quizIcone = findViewById(R.id.quizIcone);
+
+        /*mapLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent lauchIntent = new Intent(getBaseContext(),);
+                startActivity(lauchIntent);
+            }
+        });*/
+
+        View.OnClickListener encycloClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent lauchIntent = new Intent(getBaseContext(),EncyclopediaActivity.class);
+                startActivity(lauchIntent);
+
+            }
+        };
+        encycloLayout.setOnClickListener(encycloClickListener);
+        encycloIcone.setOnClickListener(encycloClickListener);
+
+        View.OnClickListener quizClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent lauchIntent = new Intent(getBaseContext(),QuizActivity.class);
+                startActivity(lauchIntent);
+            }
+        };
+        quizLayout.setOnClickListener(quizClickListener);
+        quizIcone.setOnClickListener(quizClickListener);
 
 
     }
