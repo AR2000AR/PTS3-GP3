7b76ac618858e0d663edfca06e73fa2fa9177d2c
Rémi Audrézet <AR2000AR@users.noreply.github.com>
2019-10-10 12:09:19 +0200

    Map integration

--- app/src/main/java/com/pts3/gp3/dinomap/MapActivity.java
+++ app/src/main/java/com/pts3/gp3/dinomap/MapActivity.java
@@ -4,12 +4,40 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
-public class MapActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+
+public class MapActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnInfoWindowClickListener{
+
+    GoogleMap googleMap;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onInfoWindowClick(Marker marker) {
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.googleMap=googleMap;
+
+        LatLng ceneter = new LatLng(46.603354,1.8883335);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(ceneter));
     }
 }
