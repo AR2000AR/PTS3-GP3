6c3c738e183e5fa9bb179d213a67036c98ab7e39
Daudath <azw12345@hotmail.fr>
2019-11-17 16:01:08 +0100

    avancer map

--- app/src/main/java/com/pts3/gp3/dinomap/MapActivity.java
+++ app/src/main/java/com/pts3/gp3/dinomap/MapActivity.java
@@ -2,6 +2,7 @@ package com.pts3.gp3.dinomap;
 
 import android.os.Bundle;
 import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -19,13 +20,14 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback,
     GoogleMap googleMap;
     SeekBar seekBar;
     int state;
-    String epoque;
     Toast toast;
+    TextView epoqueView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -33,6 +35,7 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback,
         mapFragment.getMapAsync(this);
 
         seekBar = findViewById(R.id.seekBarEpoque);
+        epoqueView = findViewById(R.id.epoqueView);
 
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
@@ -40,30 +43,39 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback,
                 state = progress;
                 switch (state) {
                     case 0:
+                        epoqueView.setText(R.string.cambrien);
                         Toast.makeText(getApplicationContext(), R.string.cambrien, Toast.LENGTH_SHORT).show();
                         break;
                     case 1:
+                        epoqueView.setText(R.string.ordovicien);
                         Toast.makeText(getApplicationContext(), R.string.ordovicien, Toast.LENGTH_SHORT).show();
                         break;
                     case 2:
+                        epoqueView.setText(R.string.silurien);
                         Toast.makeText(getApplicationContext(), R.string.silurien, Toast.LENGTH_SHORT).show();
                         break;
                     case 3:
+                        epoqueView.setText(R.string.devonien);
                         Toast.makeText(getApplicationContext(), R.string.devonien, Toast.LENGTH_SHORT).show();
                         break;
                     case 4:
+                        epoqueView.setText(R.string.carbonifere);
                         Toast.makeText(getApplicationContext(), R.string.carbonifere, Toast.LENGTH_SHORT).show();
                         break;
                     case 5:
+                        epoqueView.setText(R.string.permien);
                         Toast.makeText(getApplicationContext(), R.string.permien, Toast.LENGTH_SHORT).show();
                         break;
                     case 6:
+                        epoqueView.setText(R.string.trias);
                         Toast.makeText(getApplicationContext(), R.string.trias, Toast.LENGTH_SHORT).show();
                         break;
                     case 7:
+                        epoqueView.setText(R.string.jurassique);
                         Toast.makeText(getApplicationContext(), R.string.jurassique, Toast.LENGTH_SHORT).show();
                         break;
                     case 8:
+                        epoqueView.setText(R.string.cretace);
                         Toast.makeText(getApplicationContext(), R.string.cretace, Toast.LENGTH_SHORT).show();
                         break;
                 }
