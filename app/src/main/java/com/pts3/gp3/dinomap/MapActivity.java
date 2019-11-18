3d3238e8e104fa2249a715ebdd085e79512a2610
Rémi Audrézet <AR2000AR@users.noreply.github.com>
2019-11-18 11:24:05 +0100

    fix map
    - initiate dino attribute
    - wait for the map to be ready to add the markers

--- app/src/main/java/com/pts3/gp3/dinomap/MapActivity.java
+++ app/src/main/java/com/pts3/gp3/dinomap/MapActivity.java
@@ -45,7 +45,7 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback,
 
         seekBar = findViewById(R.id.seekBarEpoque);
         epoqueView = findViewById(R.id.epoqueView);
-        List<Dino> dino = new ArrayList<>();
+        dino = new ArrayList<>();
 
         /**
          * rempli la liste avec des dino
@@ -61,8 +61,6 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback,
 
         state=seekBar.getProgress();
         epoque=epoqueChoice();
-
-        afficherMarqueur(dino,epoque);
 
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
@@ -87,38 +85,38 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback,
         switch (state) {
             case 0:
                 epoqueView.setText(R.string.cambrien);
-                return ""+ R.string.cambrien;
+                return getString(R.string.cambrien);
             case 1:
                 epoqueView.setText(R.string.ordovicien);
-                return ""+ R.string.ordovicien;
+                return getString(R.string.ordovicien);
             case 2:
                 epoqueView.setText(R.string.silurien);
-                return ""+ R.string.silurien;
+                return getString(R.string.silurien);
             case 3:
                 epoqueView.setText(R.string.devonien);
-                return ""+ R.string.devonien;
+                return getString(R.string.devonien);
             case 4:
                 epoqueView.setText(R.string.carbonifere);
-                return ""+ R.string.carbonifere;
+                return getString(R.string.carbonifere);
             case 5:
                 epoqueView.setText(R.string.permien);
-                return ""+ R.string.permien;
+                return getString(R.string.permien);
             case 6:
                 epoqueView.setText(R.string.trias);
-                return ""+ R.string.trias;
+                return getString(R.string.trias);
             case 7:
                 epoqueView.setText(R.string.jurassique);
-                return ""+ R.string.jurassique;
+                return getString(R.string.jurassique);
             case 8:
                 epoqueView.setText(R.string.cretace);
-                return ""+ R.string.cretace;
+                return getString(R.string.cretace);
         }
         return null;
     }
 
     private void afficherMarqueur(List<Dino> list, String epoque){
         for (Dino d: list) {
-            if(epoque == d.getEpoque()){
+            if (epoque.equals(d.getEpoque())) {
                 for(LatLng l : d.getLieuDeDecouverte()) {
                     googleMap.addMarker(new MarkerOptions().position(l).title(""));
                 }
@@ -141,6 +139,8 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback,
 
         LatLng center = new LatLng(46.603354, 1.8883335);
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(center));
+
+        afficherMarqueur(dino, epoque);
     }
 
     public List<Dino> getDino() {
