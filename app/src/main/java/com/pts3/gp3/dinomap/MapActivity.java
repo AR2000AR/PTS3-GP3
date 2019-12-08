bbc83e47b51ae04ffb1fcd04526c45bd1b74cae6
ValentinROUXEL <vrouxellycee@gmail.com>
2019-12-08 13:53:06 +0100

    AJOUT BUBBLE BOX

--- app/src/main/java/com/pts3/gp3/dinomap/MapActivity.java
+++ app/src/main/java/com/pts3/gp3/dinomap/MapActivity.java
@@ -1,6 +1,10 @@
 package com.pts3.gp3.dinomap;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -32,6 +36,9 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback,
     private List<Dino> dino;
     private String epoque;
 
+    private TextView t;
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,9 +50,22 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback,
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+
+        //Texte qui va apparaitre par dessus la seek bar quand on va deplacer le curseur
+
+
         seekBar = findViewById(R.id.seekBarEpoque);
         epoqueView = findViewById(R.id.epoqueView);
         dino = new ArrayList<>();
+
+
+       t = findViewById(R.id.t);
+        t.setTextSize(15);
+        t.setX(0);
+
+
+        //layoutSeekBar.addView(t);
+
         /**
          * rempli la liste avec des dino
          */
@@ -77,38 +97,63 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback,
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
                 afficherMarqueur(getDino(),epoque);
+                t.setText("");
             }
         });
+
+        seekBar.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+
+                t.setX((float) (motionEvent.getX() - 0.5*(t.getWidth())));
+                return false;
+            }
+        });
+    }
+
+    private void mettreAJourTexteIndicateur(int progress) {
+
+
+
     }
 
     private String epoqueChoice() {
         switch (state) {
             case 0:
                 epoqueView.setText(R.string.cambrien);
+                t.setText("-540M à \n -500M");
                 return getString(R.string.cambrien);
             case 1:
                 epoqueView.setText(R.string.ordovicien);
+                t.setText("-500M à \n -440M");
                 return getString(R.string.ordovicien);
             case 2:
                 epoqueView.setText(R.string.silurien);
+                t.setText("-440M à \n -410M");
                 return getString(R.string.silurien);
             case 3:
                 epoqueView.setText(R.string.devonien);
+                t.setText("-410M à \n -355M");
                 return getString(R.string.devonien);
             case 4:
                 epoqueView.setText(R.string.carbonifere);
+                t.setText("-355M à \n -295M");
                 return getString(R.string.carbonifere);
             case 5:
                 epoqueView.setText(R.string.permien);
+                t.setText("-295M à \n -250M");
                 return getString(R.string.permien);
             case 6:
                 epoqueView.setText(R.string.trias);
+                t.setText("-250M à \n -200M");
                 return getString(R.string.trias);
             case 7:
                 epoqueView.setText(R.string.jurassique);
+                t.setText("-200M à \n -135M");
                 return getString(R.string.jurassique);
             case 8:
                 epoqueView.setText(R.string.cretace);
+                t.setText("-135M à \n -65M");
                 return getString(R.string.cretace);
         }
         return null;
