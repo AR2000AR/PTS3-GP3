0e1f0917c07bf872f70075005108f190c12d2307
Rémi Audrézet <AR2000AR@users.noreply.github.com>
2020-01-13 00:22:39 +0100

    add Intent flag FLAG_ACTIVITY_REORDER_TO_FRONT to prevent weird return button behaviour

--- app/src/main/java/com/pts3/gp3/dinomap/CreditActivity.java
+++ app/src/main/java/com/pts3/gp3/dinomap/CreditActivity.java
@@ -19,7 +19,7 @@ public class CreditActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(CreditActivity.this, TutorielActivity.class));
+                startActivity(new Intent(CreditActivity.this, TutorielActivity.class).setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT));
             }
         });
     }
